f6302bc715266f66d159f27185baea732b1f11c1
isaacbell1017 <isaacbell1017@gmail.com>
2023-02-06 21:48:30 -0600

    wip

--- cppsrc/workers/Client.hpp
+++ cppsrc/workers/Client.hpp
@@ -1,39 +1,62 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <memory>
 
 #include "PocoHandler.hpp"
 
 namespace Workers
 {
-  template <class WorkPolicy>
+  // template <class WorkPolicy>
   class Client
   {
   public:
-    static void run()
+    static Client& getInstance()
     {
-      PocoHandler handler("127.0.0.1", 5672);
+      static Client instance;
+      return instance;
+    }
 
-      // TODO - add env var for login credentials
-      AMQP::Connection connection(&handler, AMQP::Login("guest", "guest"), "/");
-      AMQP::Channel channel(&connection);
+    void run()
+    {
+      if (!isRunning_)
+      {
+        PocoHandler handler("127.0.0.1", 5672);
 
-      channel.onReady([&]()
-                      { std::cout << "Worker is ready!" << std::endl; });
+        // TODO - add env var for login credentials
+        AMQP::Connection connection(&handler, AMQP::Login("guest", "guest"), "/");
+        AMQP::Channel channel(&connection);
 
-      channel.declareExchange("ts-exchange", AMQP::direct);
-      channel.declareQueue("ts-generic-response");
-      channel.bindQueue("ts-exchange", "ts-generic-response", "generic-response");
-      channel
-          .consume("ts-generic-request", AMQP::noack)
-          .onReceived(
-              [&channel](const AMQP::Message &message, uint64_t deliveryTag, bool redelivered)
-              { WorkPolicy.execute(channel, message, deliveryTag, redelivered); });
+        channel.onReady([&]()
+                        { std::cout << "Worker is ready!" << std::endl; });
 
-      handler.loop();
+        channel.declareExchange("ts-exchange", AMQP::direct);
+        channel.declareQueue("ts-generic-response");
+        channel.bindQueue("ts-exchange", "ts-generic-response", "generic-response");
+        channel
+            .consume("ts-email-send", AMQP::noack)
+            .onReceived(
+                [&channel](const AMQP::Message &message, uint64_t deliveryTag, bool redelivered)
+                { WorkPolicy.execute(channel, message, deliveryTag, redelivered); });
+
+        channel
+            .consume("ts-generic-request", AMQP::noack)
+            .onReceived(
+                [&channel](const AMQP::Message &message, uint64_t deliveryTag, bool redelivered)
+                { WorkPolicy.execute(channel, message, deliveryTag, redelivered); });
+
+        handler.loop();
+        isRunning_ = true;
+      }
     }
 
   private:
+    // Singleton
+    Client() = default;
+    Client(const Client&) = delete;
+    Client& operator=(const Client&) = delete;
+
+    bool isRunning_ = false;
     Poco::Net::HTTPCredentials auth_;
   };
 }
